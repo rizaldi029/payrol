9941412986ec7d31ddd83da85138371b905eb3e6
rizaldi029 <86280990+rizaldi029@users.noreply.github.com>
2022-06-11 19:43:42 +0700

    Update home.php

--- application/views/pegawai/home.php
+++ application/views/pegawai/home.php
@@ -10,12 +10,14 @@
             <!-- Map card -->
             <div class="card"> 
               <div class="card-header"> Notifikasi </h3>
-              </div>
-              <form method="post" action="pegawai/proses_absen">
+              <form action="" enctype="multipart/form-data  ">
+
+              </form>
+              <form="post" action="pegawai/proses_absen">
                <div class="card-body">
                   <?php if ($waktu != 'dilarang') { ?>
                   <p class="text-center">Hai, <?=$this->session->userdata('nama')?> anda hari ini belum melakukan absen <b><?=$waktu?></b>. Silahkan lakukan absen pada tombol abse berikut <br><br><button class="btn btn-primary" >Absen <?=$waktu?></button></p>
-                  <input type="hidden" name="ket" id="ket" value="<?=$waktu?>">
+                  <input type="file" name="userfile" class="form-control" id="ket" value="<?=$waktu?>">
                   <?php }else{ ?>
                   <p class="text-center">Hai, <?=$this->session->userdata('nama')?> anda hari ini sudah melakukan absensi <b>Masuk</b> dan <b>Pulang</b></p>
                   <?php }  ?>
